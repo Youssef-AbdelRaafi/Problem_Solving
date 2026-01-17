1824c83219ebf0437e3b4e9ea96181e9236fa95c
Youssef-AbdelRaafi <yousef4221117@gmail.com>
2026-01-17 20:59:39 +0200

    Add Fixed Sliding Window solutions

---
+++ Fixed_Sliding_Window/MaximumNumberOfVowelsInASubstringOfGivenLength/MaximumNumberOfVowelsInASubstringOfGivenLength/Program.cs
@@ -0,0 +1,81 @@
+﻿/*
+Given a string s and an integer k, return the maximum number of vowel letters in any substring of s with length k.
+
+Vowel letters in English are 'a', 'e', 'i', 'o', and 'u'.
+
+ 
+
+Example 1:
+
+Input: s = "abciiidef", k = 3
+Output: 3
+Explanation: The substring "iii" contains 3 vowel letters.
+Example 2:
+
+Input: s = "aeiou", k = 2
+Output: 2
+Explanation: Any substring of length 2 contains 2 vowels.
+Example 3:
+
+Input: s = "leetcode", k = 3
+Output: 2
+Explanation: "lee", "eet" and "ode" contain 2 vowels.
+ 
+
+Constraints:
+
+1 <= s.length <= 105
+s consists of lowercase English letters.
+1 <= k <= s.length
+*/
+
+string input = "";
+
+Console.WriteLine("Please Enter a string?");
+
+input = Console.ReadLine() ?? "";
+
+int windowSize = 3;
+
+
+Console.WriteLine($"Maximum Numbers Of Vowels: {(MaximumNumbersOfVowels(input, windowSize))}");
+
+//Fixed Sliding Window
+
+int MaximumNumbersOfVowels(string input, int windowSize)
+{
+    int maxNumber = 0;
+    int windowSum = 0;
+
+    for (int i = 0; i < windowSize; i++)
+    {
+        if (IsVowel(input[i]))
+        {
+            windowSum++;
+        }
+    }
+
+    maxNumber = windowSum;
+
+    for (int i = windowSize; i < input.Length; i++)
+    {
+        if(IsVowel(input[i-windowSize]))
+        {
+            windowSum--;
+        }
+
+        if (IsVowel(input[i]))
+        {
+            windowSum++;
+        }
+
+        maxNumber=Math.Max(maxNumber, windowSum);
+    }
+
+    return maxNumber;
+}
+
+bool IsVowel(char c)
+{
+    return c == 'a' || c == 'o' || c == 'u' || c == 'i' || c == 'e';
+}
---
+++ Fixed_Sliding_Window/MaximumSumOrAverageOfSlidingWindowOf3Elements/MaximumSumOrAverageOfSlidingWindowOf3Elements/Program.cs
@@ -0,0 +1,47 @@
+﻿//Find The Maximum Sum Or Avearage Of a Sliding Window Of 3 elements.
+
+int[] numbers = { 4, 2, 7, 1, 5, 3, 6, 2, 8, 1 };
+int windowSize = 3;
+
+Console.WriteLine($"MaxSumWithBruteForce:{(MaxSumWithBruteForce(numbers, windowSize))}");
+Console.WriteLine($"MaxSumWithBruteForce:{(MaxSumWithFixedSlidingWindow(numbers, windowSize))}");
+
+int MaxSumWithFixedSlidingWindow(int[] source,int windowSize)
+{
+    int maxValue = 0, windowSum = 0;
+
+    for (int i = 0; i < windowSize; i++) 
+    {
+        windowSum += source[i];
+    }
+
+    maxValue = windowSum;
+
+    for (int i = windowSize; i < source.Length; i++)
+    {
+        windowSum += source[i] - source[i - windowSize];
+
+        maxValue = Math.Max( maxValue, windowSum );
+    }
+
+    return maxValue;
+}
+
+int MaxSumWithBruteForce(int[] source, int windowSize)
+{
+    int maxValue = int.MinValue;
+
+    for (int i = 0; i <= source.Length - windowSize; i++)
+    {
+        int windowSum = 0;
+
+        for (int j = i; j < i + windowSize; j++)
+        {
+            windowSum += source[j];
+        }
+
+        maxValue = Math.Max(maxValue, windowSum);
+    }
+
+    return maxValue;
+}
