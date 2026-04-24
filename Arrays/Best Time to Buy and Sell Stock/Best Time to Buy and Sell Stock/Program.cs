9951f28a307127d27fa8913cf0f5f52b39a0b25f
Youssef-AbdelRaafi <yousef4221117@gmail.com>
2026-04-24 16:36:09 +0300

    feat: Add LeetCode Array problems - Two Sum, Best Time to Buy and Sell Stock, Product of Array Except Self, Remove Duplicates from Sorted Array

---
+++ Arrays/Best Time to Buy and Sell Stock/Best Time to Buy and Sell Stock/Program.cs
@@ -0,0 +1,27 @@
+﻿int MaximumProfit(int [] prices)
+{
+    int minPrice = int.MaxValue;
+    int maxProfit = 0;
+
+    for (int i = 0; i < prices.Length; i++)
+    {
+        if (prices[i] < minPrice)
+        {
+            minPrice = prices[i];
+        }
+
+        else
+        {
+            int profit = prices[i] - minPrice;
+
+            if(profit>maxProfit)
+            {
+                maxProfit = profit;
+            }
+        }
+    }
+
+    return maxProfit;
+}
+
+Console.WriteLine(MaximumProfit([7, 1, 5, 3, 6, 4]));
---
+++ Arrays/Two Sum/Two Sum/Program.cs
@@ -0,0 +1,30 @@
+﻿int [] TwoSum(int[]num,int target)
+{
+    Dictionary<int, int> keyValues = new Dictionary<int, int> ();
+
+    int n = num.Length;
+    int [] arr = new int [n];
+    for (int i = 0; i < n; i++) 
+    {
+
+        int result = target - num[i];
+
+        if(keyValues.ContainsKey(result))
+        {
+           arr = new int[] { keyValues[result], i };
+        }
+
+        else
+        {
+            keyValues.Add(num[i], i);
+        }
+    }
+
+    return arr;
+}
+
+int [] arr =TwoSum([2, 7, 11, 15], 9);
+for (int i = 0; i < arr.Length; i++) 
+{
+    Console.WriteLine(arr[i]);
+}
